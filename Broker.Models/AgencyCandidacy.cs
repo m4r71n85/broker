45dba0959cd5c51a967d866f10cfb15a18c5bd3e
m4r71n85 <martin.tsekov.bg@gmail.com>
2014-10-26 03:50:27 +0300

    same as previous

---
+++ Broker.Models/AgencyCandidacy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Broker.Models
+{
+    public class AgencyCandidacy
+    {
+        public int Id { get; set; }
+        
+        [Index(IsUnique = true)]
+        public ApplicationUser Candidator { get; set; }
+        public Agency Agency { get; set; }
+    }
+}
